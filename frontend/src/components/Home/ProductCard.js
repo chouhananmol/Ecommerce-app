49c0add581aae177b30245aa30985a96e96676ec
Anmol chouhan <anmolchouhan81@gmail.com>
2024-08-18 23:45:01 +0530

    [MOD] Resolved initial price now as 1 intead of 0

--- frontend/src/components/Home/ProductCard.js
+++ frontend/src/components/Home/ProductCard.js
@@ -4,23 +4,23 @@ import { Rating } from "@mui/material";
 import ProductAlt from "../../images/logo2.jpeg"
 
 
-const ProductCard = ({product}) => {
+const ProductCard = ({ product }) => {
   const options = {
-      size: "large",
-      value: product.ratings,
-      readOnly: true,
-      precision: 0.5,
-    };
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
 
 
   return (
-   <Link className="productCard" to={`/product/${product._id}`}>
-<img src={(product?.images[0])?product?.images[0]?.url:ProductAlt} alt ={product.name} />
-    <p>{product.name}</p>
-    <div>
-<Rating {...options} /><span className='productCardSpan'>({product.numOfReviews} reviews)</span>
-    </div>
-    <span style={{color:"#001a14"}}>{`$${product.price}`}</span>
+    <Link className="productCard" to={`/product/${product._id}`}>
+      <img src={(product?.images[0]) ? product?.images[0]?.url : ProductAlt} alt={product.name} />
+      <p>{product.name}</p>
+      <div>
+        <Rating {...options} /><span className='productCardSpan'>({product.numOfReviews} reviews)</span>
+      </div>
+      <span style={{ color: "#001a14" }}>{`$${product.price}`}</span>
     </Link>
   )
 }
